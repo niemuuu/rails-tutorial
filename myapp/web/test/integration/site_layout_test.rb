8f51e2128a87797db153b5cef93474040296a3bd
Niemuuu <yorori.too@gmail.com>
2019-05-11 20:48:46 +0900

    create users page

--- myapp/web/test/integration/site_layout_test.rb
+++ myapp/web/test/integration/site_layout_test.rb
@@ -1,20 +1,40 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test "layouts links" do
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+
 
     get contact_path
     assert_select "title", full_title("Contact")
 
     get signup_path
     assert_select "title", full_title("Sign up")
+  end
+
+  test "layout links in case of logging in" do
+    log_in_as @user
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 
 end
