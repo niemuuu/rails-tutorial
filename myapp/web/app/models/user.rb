40fe3943cd87bad6be5c5b7ba26e13194ddfade9
Niemuuu <yorori.too@gmail.com>
2019-05-12 17:36:47 +0900

    impl reset password update action

--- myapp/web/app/models/user.rb
+++ myapp/web/app/models/user.rb
@@ -66,7 +66,13 @@ class User < ApplicationRecord
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
-  end  
+  end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+  
 
   private
 
